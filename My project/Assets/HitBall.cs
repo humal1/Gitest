7f1b1190af83e7955b4a6015cfb62dfe0c21a0c9
humal1 <IvelinDimitrov@outlook.com>
2022-02-10 18:19:46 +0000

    Codes from my project
    Codes project for game board

---
+++ My project/Assets/HitBall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+        rigidBody = GetComponent<Rigidbody>();
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float f = Input.GetAxis("Vertical");
+        Vector3 force = new Vector3(2*h, 2*f);
+        rigidBody.AddForce(force);
+        
+    }
+}
